5071a2377d5cb6e669bd3c5bb1a668cb7406b490
Po-Yu SHIH <kevin.shih.fr@gapp.nthu.edu.tw>
2020-10-24 15:43:50 +0800

    update dependencies

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,5 @@
 import React , {Component} from 'react';
 import { Container } from 'semantic-ui-react';
-import Head from 'next/head';
 import Header from './Navibar';
 
 class Layout extends Component {
@@ -8,9 +7,7 @@ class Layout extends Component {
     render() {
         return (
             <Container>
-                <Head>
-                    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-                </Head>
+                <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
                 <Header />
                 {this.props.children}
             </Container>
