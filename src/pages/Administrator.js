6fc3eccda325ec49aa8c648c2d53a215e4406eb9
Po-Yu SHIH <kevin.shih.fr@gapp.nthu.edu.tw>
2020-10-24 23:53:49 +0800

    conflict fix

--- src/pages/Administrator.js
+++ src/pages/Administrator.js
@@ -1,13 +1,8 @@
 import React , {Component} from 'react';
 import Layout from '../components/Layout';
 import {Header, Grid} from 'semantic-ui-react';
-<<<<<<< HEAD:src/pages/Administrator.js
-import Conference from '../Conference';
+import YOYO from '../YOYO';
 import AdminFuncHelper from './AdminFuncList';
-=======
-import Conference from './Conference';
-import YOYO from './YOYO';
->>>>>>> origin/api_create:src/Administrator.js
 
 
 class Administrator extends Component {
