3b57326006c84af13f956faf5482fa84ccbb5bb8
106062205 <a940139kevin@gmail.com>
2020-10-25 04:21:56 +0800

    change router and add group function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,82 @@
 import 'semantic-ui-css/semantic.min.css'
 import React , {Component} from 'react';
-import { Route } from 'react-router-dom';
 import Administrator from './pages/Administrator';
 import Participant  from './pages/Participant';
+import {database} from "./firebase/";
 //import VideoConference from './VideoConference';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props)
+    this.state={
+      group1:0,
+      group2:0,
+      logiGroup:0,
+    }
     
+  }
+  setGroupCount=async(gName,op)=>{
+    let num;
+    await database.ref(gName).once('value').then(e=>{
+      num=e.val().num;
+    })
+    if(op==='-'){
+      await database.ref(gName).set({num:num-1})
+    }
+    else{
+      await database.ref(gName).set({num:num+1})
+    }
+    
+  }
+  componentDidMount(){
+    database.ref('group1').on('value',e=>{
+      this.setState({group1:e.val().num});
+    });
+    database.ref('group2').on('value',e=>{
+      this.setState({group2:e.val().num});
+    });
+    database.ref('logiGroup').on('value',e=>{
+      this.setState({logiGroup:e.val().num});
+    });
+
+  }
+  render() {
     return (
       <div className="container">
-        {/* The corresponding component will show here if the current URL matches the path */}
-        <Route path="/" exact component={Administrator} />
-        <Route path="/admin" component={Administrator} />
-        <Route path="/participant" component={Participant} />
-        {/* <Route path="/speech" component={Speech} /> */}
+        <React.Fragment>
+          <Router>
+            <Switch>
+              <Route path="/">
+                <Administrator 
+                  group1={this.state.group1} 
+                  group2={this.state.group2} 
+                  logiGroup={this.state.logiGroup}/>
+              </Route>
+              <Route path="/admin">
+                <Administrator 
+                  group1={this.state.group1} 
+                  group2={this.state.group2} 
+                  logiGroup={this.state.logiGroup}/>
+              </Route>
+              <Route path="/participant">
+                <Participant  
+                  group1={this.state.group1} 
+                  group2={this.state.group2} 
+                  logiGroup={this.state.logiGroup}/>
+              </Route>
+            </Switch>
+          </Router>
+        </React.Fragment>
+        {/* <Route path="/" component={(props)=>{}} />
+        <Route path="/admin" component={(props)=>{<Administrator group1={this.state.group1} group2={this.state.group2} logiGroup={this.state.logiGroup}/>}}/>
+        <Route path="/participant" component={(props)=>{<Participant  group1={this.state.group1} group2={this.state.group2} logiGroup={this.state.logiGroup}/>}} /> */}
       </div>
     );
   }
